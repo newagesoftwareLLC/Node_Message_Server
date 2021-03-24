de6b18bff03ea430e7aeae1d3a58454c409fe820
Jacob Fliss <newgmobile@gmail.com>
2021-03-24 12:36:11 -0400

    Update windows-install-service.bat

--- server/windows-install-service.bat
+++ server/windows-install-service.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 :choice
-set /P c=IMPORTANT: Did you edited the install-windows-service.js file to reflect the location of the message.js file?[Y/N]?
+set /p c=[!] Did you edited the install-windows-service.js file for the location of message.js? [Y/N]: 
 if /I "%c%" EQU "Y" goto :yes
 if /I "%c%" EQU "N" goto :no
 goto :choice
@@ -9,8 +9,8 @@ goto :choice
 :yes
 npm install -g node-windows
 npm link node-windows
-echo "DONE. Check the Windows services form to be sure it is installed."
+echo DONE. Check the Windows services form to be sure it is installed.
 
 :no
-echo "Go edit install-windows-service.js with Notepad and come back to this batch file."
+echo Go edit install-windows-service.js with Notepad and come back to this batch file.
 pause
